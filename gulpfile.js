4380adb4fabc5e81e3a3eec0756b0eecb56ace4f
June <june@cynder.io>
2016-03-07 12:25:08 +0800

    Code clean-up; edit styles without refreshing the page; changed command to 'gulp serve'

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,34 @@
-var gulp         = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync  = require('browser-sync');
-var concat       = require('gulp-concat');
-var connect      = require('gulp-connect');
-var clean        = require('gulp-clean');
-var gutil        = require('gulp-util');
-var imagemin     = require('gulp-imagemin')
-var jade         = require('gulp-jade');
-var rename       = require('gulp-rename');
-var minifyCSS    = require('gulp-minify-css');
-var notify       = require('gulp-notify');
-var plumber      = require('gulp-plumber');
-var runSequence  = require('run-sequence');
-var shell        = require('gulp-shell');
-var sourcemaps   = require('gulp-sourcemaps');
-var stylus       = require('gulp-stylus');
-var uglify       = require('gulp-uglify');
+var gulp         = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync  = require('browser-sync').create(),
+    concat       = require('gulp-concat'),
+    del          = require('del'),
+    imagemin     = require('gulp-imagemin'),
+    jade         = require('gulp-jade'),
+    rename       = require('gulp-rename'),
+    minifyCSS    = require('gulp-minify-css'),
+    notify       = require('gulp-notify'),
+    plumber      = require('gulp-plumber'),
+    runSequence  = require('run-sequence'),
+    shell        = require('gulp-shell'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    stylus       = require('gulp-stylus'),
+    uglify       = require('gulp-uglify');
+
+var sourcePath = {
+        jade:   'app/templates/',
+        stylus: 'app/assets/styl/',
+        js:     'app/assets/js/',
+        img:    'app/assets/img/',
+        fonts:  'app/assets/fonts/'
+    },
+    destPath = {
+        html:  'public/',
+        css:   'public/assets/css/',
+        js:    'public/assets/js/',
+        img:   'public/assets/img/',
+        fonts: 'public/assets/fonts/'
+    };
 
 var reportError = function(error){
     notify.onError({
@@ -27,99 +40,90 @@ var reportError = function(error){
     this.emit('end');
 };
 
-gulp.task('html', function() {
-    gulp.src('app/**/!(_)*.jade')
+gulp.task('jade', function() {
+    gulp.src(sourcePath.jade + '**/!(_)*.jade')
         .pipe(plumber({ errorHandler: reportError }))
         .pipe(jade({ pretty: true }))
         .pipe(rename({ dirname: './' }))
-        .pipe(gulp.dest('public'))
-        .pipe(connect.reload())
+        .pipe(gulp.dest(destPath.html))
         .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('css', function() {
-    gulp.src('app/assets/styl/main.styl')
+gulp.task('stylus', function() {
+    gulp.src(sourcePath.stylus + 'main.styl')
         .pipe(plumber({ errorHandler: reportError }))
-        .pipe(stylus())
         .pipe(sourcemaps.init())
+        .pipe(stylus())
         .pipe(autoprefixer())
         .pipe(rename('main.css'))
-        .pipe(gulp.dest('public/assets/css'))
+        .pipe(gulp.dest(destPath.css))
         .pipe(rename('main.min.css'))
         .pipe(minifyCSS())
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('public/assets/css'))
-        .pipe(connect.reload())
-        .pipe(browserSync.reload({ stream: true }));
+        .pipe(gulp.dest(destPath.css));
 });
 
 gulp.task('js', function() {
-    gulp.src('app/assets/js/**/*.js')
+    gulp.src(sourcePath.js + '**/*.js')
         .pipe(plumber({ errorHandler: reportError }))
         .pipe(sourcemaps.init())
         .pipe(concat('main.min.js'))
         .pipe(uglify())
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('public/assets/js'));
+        .pipe(gulp.dest(destPath.js))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('images', function() {
-    gulp.src('app/assets/img/**/*')
+gulp.task('img', function() {
+    gulp.src(sourcePath.img + '**/*')
         .pipe(plumber({ errorHandler: reportError }))
         .pipe(imagemin({
             optimizationLevel: 3,
             progressive: true,
             interlaced: true
         }))
-        .pipe(gulp.dest('public/assets/img'));
+        .pipe(gulp.dest(destPath.img));
 });
 
 gulp.task('copyfonts', function() {
-    gulp.src('app/assets/fonts/**/*.{ttf,otf,woff,woff2,eof,svg}')
-        .pipe(gulp.dest('public/assets/fonts'));
+    gulp.src(sourcePath.fonts + '**/*.{ttf,otf,woff,woff2,eof,svg}')
+        .pipe(gulp.dest(destPath.fonts));
 });
 
-gulp.task('connect', function() {
-    connect.server({
-        root: 'public',
-        livereload: true
-    });
-});
-
-gulp.task('browser-sync', function() {
+gulp.task('browserSyncConfig', function() {
     browserSync.init({
-        server: {
-            baseDir: 'public'
-        }
+        port: 6969,
+        server: './public',
+        notify: false,
+        files: [
+            destPath.css + '**/*.css'
+        ]
     });
 });
 
 gulp.task('watch', function() {
-    gulp.watch('app/**/*.jade', ['html']);
-    gulp.watch('app/assets/**/*.styl', ['css']);
-    gulp.watch('app/assets/**/*.js', ['js']);
-    gulp.watch('app/assets/**/*.{jpg,png,gif}', ['images']);
+    gulp.watch(sourcePath.jade + '**/*.jade', ['jade']);
+    gulp.watch(sourcePath.stylus + '**/*.styl', ['stylus']);
+    gulp.watch(sourcePath.js + '**/*.js', ['js']);
+    gulp.watch(sourcePath.img + '**/*.{jpg,png,gif}', ['img']);
 });
 
 gulp.task('clean-build', function() {
-    gulp.src([
-        'public/assets/css/main.css',
-        'public/assets/css/main.min.css',
-        'public/assets/css/main.min.css.map',
-        'public/assets/js/main.min.js',
-        'public/assets/js/main.min.js.map',
-        'public/assets/fonts/**/*.{ttf,woff,woff2,eof,svg}',
-        'public/**/*.html',
+    return del([
+        destPath.html + '*.html',
+        destPath.css + 'main.css',
+        destPath.css + 'main.min.css',
+        destPath.css + 'main.min.css.map',
+        destPath.js + 'main.min.js',
+        destPath.js + 'main.min.js.map',
+        destPath.fonts + '*'
     ])
-        .pipe(clean());
 });
 
-gulp.task('build', ['html', 'css', 'js', 'images', 'copyfonts']);
-
+gulp.task('build', ['jade', 'stylus', 'js', 'img', 'copyfonts']);
 gulp.task('re-build', function(callback) {
     runSequence('clean-build', ['build'], callback);
 });
 
-gulp.task('start', ['build', 'connect', 'watch']);
-gulp.task('start-browser-sync', ['build', 'browser-sync', 'watch']);
+gulp.task('serve', ['browserSyncConfig', 'build', 'watch']);
 gulp.task('default');
